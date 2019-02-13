18f484688d29d4643bde1b8ea3fad4fe73ee3b2b
Nicolas Slythe <nicolas@slythe.net>
2019-02-13 14:52:19 -0500

    cleaning after unittest

--- OpenGameServer/Global.py
+++ OpenGameServer/Global.py
@@ -4,9 +4,9 @@ from OpenGameServer import ConfigObject
 
 class Global(object):
     def __init__(self):
+        self.initialized = False
         self.ConfigPath = "run/etc/ogs"
         self.ConfigFileName = "global.json"
-        self.ConfigFilePath = os.path.join(self.ConfigPath, self.ConfigFileName)
 
         self.config =ConfigObject.ConfigDict({
             "DataPath" : "run/data",
@@ -14,29 +14,48 @@ class Global(object):
             "ServersDataPath" : "run/servers/data"
         })
 
+    def init(self):
+        self.initialized = True
+        os.makedirs(self.DataPath, mode=0o777, exist_ok = True)
+        os.makedirs(self.ServersConfigPath, mode=0o777, exist_ok = True)
+        os.makedirs(self.ServersDataPath, mode=0o777, exist_ok = True)
+
+        self.ConfigFilePath = os.path.join(self.ConfigPath, self.ConfigFileName)
         self.configFile = ConfigObject.Config(self.ConfigFilePath, self.config)
         if not os.path.exists(self.configFile.filePath):
             self.configFile.save()
         else:
             self.configFile.load()
 
-        os.makedirs(self.DataPath, mode=0o777, exist_ok = True)
-        os.makedirs(self.ServersConfigPath, mode=0o777, exist_ok = True)
-        os.makedirs(self.ServersDataPath, mode=0o777, exist_ok = True)
-
-
-
     @property
     def DataPath(self):
+        if not self.initialized:
+            raise Exception("Not initialized")
         return self.config["DataPath"].get()
+
+    @DataPath.setter
+    def DataPath(self, value):
+        return self.config["DataPath"].set(value)
 
     @property
     def ServersConfigPath(self):
+        if not self.initialized:
+            raise Exception("Not initialized")
         return self.config["ServersConfigPath"].get()
+
+    @ServersConfigPath.setter
+    def ServersConfigPath(self, value):
+        return self.config["ServersConfigPath"].set(value)
 
     @property
     def ServersDataPath(self):
+        if not self.initialized:
+            raise Exception("Not initialized")
         return self.config["ServersDataPath"].get()
+
+    @ServersDataPath.setter
+    def ServersDataPath(self, value):
+        return self.config["ServersDataPath"].set(value)
 
 config = Global()
 
--- test.py
+++ test.py
@@ -1,8 +1,9 @@
 #! /usr/bin/env python3
 
-import unittest
+import unittest, shutil
+from OpenGameServer import Global
 
-def load_tests(loader, tests, pattern):
+def suite():
     suite = unittest.TestSuite()
     for all_test_suite in unittest.defaultTestLoader.discover('test', pattern='*.py'):
         for test_suite in all_test_suite:
@@ -10,4 +11,13 @@ def load_tests(loader, tests, pattern):
     return suite
 
 if __name__ == '__main__':
-    unittest.main()
+    Global.config.ConfigPath = "run_test/etc/ogs"
+    Global.config.DataPath = "run_test/data"
+    Global.config.ServersConfigPath = "run_test/servers/config"
+    Global.config.ServersDataPath = "run_test/servers/data"
+    Global.config.init()
+
+    runner = unittest.TextTestRunner()
+    runner.run(suite())
+
+    shutil.rmtree("run_test")
